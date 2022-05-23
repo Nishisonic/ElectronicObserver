c129e46609f663a1700c59040fc01121922667e3
gre4bee <gre4bee@gmail.com>
2022-05-23 21:46:28 +0800

    Add WPF Log
    --Add WPF logview
    --Autoscrolls to last item

---
+++ ElectronicObserver/Window/Wpf/Log/LogView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElectronicObserver.Window.Wpf.Log;
+/// <summary>
+/// Interaction logic for LogView.xaml
+/// </summary>
+public partial class LogView : UserControl
+{ 
+	public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+"ViewModel", typeof(LogViewViewModel), typeof(LogView), new PropertyMetadata(default(LogViewViewModel)));
+
+	public LogViewViewModel ViewModel
+	{
+		get => (LogViewViewModel)GetValue(ViewModelProperty);
+		set => SetValue(ViewModelProperty, value);
+	}
+
+	public LogView()
+	{
+		InitializeComponent();
+	}
+
+	private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+	{
+		if (e.ExtentHeightChange != 0)
+		{
+			ScrollViewer.ScrollToVerticalOffset(ScrollViewer.ExtentHeight);
+		}
+	}
+}
