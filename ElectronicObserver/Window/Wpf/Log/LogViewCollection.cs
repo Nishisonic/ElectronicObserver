b5196f963cdcc14b3bc2f6f08f4e7f016b0a7c8d
gre4bee <gre4bee@gmail.com>
2022-05-23 21:46:28 +0800

    Add WPF Log
    --Add WPF logview
    --Autoscrolls to last item

---
+++ ElectronicObserver/Window/Wpf/Log/LogViewCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicObserver.Window.Wpf.Log;
+public class LogViewCollection : ObservableCollection<string>
+{
+}
---
+++ ElectronicObserver/Window/Wpf/Log/LogViewViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using ElectronicObserver.Resource;
+using ElectronicObserver.Utility;
+using ElectronicObserver.ViewModels;
+using ElectronicObserver.ViewModels.Translations;
+namespace ElectronicObserver.Window.Wpf.Log;
+public partial class LogViewViewModel : AnchorableViewModel
+{
+	public FormLogTranslationViewModel FormLog { get; set; }
+	public LogViewCollection LogList { get; set; } = new();
+	public LogViewViewModel() : base("Log", "Log",
+		ImageSourceIcons.GetIcon(IconContent.FormLog))
+	{
+		FormLog = Ioc.Default.GetService<FormLogTranslationViewModel>()!;
+
+		foreach (var log in Utility.Logger.Log)
+		{
+			if (log.Priority >= Utility.Configuration.Config.Log.LogLevel)
+				LogList.Add(log.ToString());
+		}
+		Utility.Logger.Instance.LogAdded += new LogAddedEventHandler((Utility.Logger.LogData data) =>
+		{
+			if (!Dispatcher.CurrentDispatcher.Thread.Equals(Thread.CurrentThread))
+			{
+				//Invokeはメッセージキューにジョブを投げて待つので、別のBeginInvokeされたジョブが既にキューにあると、
+				// それを実行してしまい、BeginInvokeされたジョブの順番が保てなくなる
+				// GUIスレッドによる処理は、順番が重要なことがあるので、GUIスレッドからInvokeを呼び出してはいけない
+				Dispatcher.CurrentDispatcher.Invoke(new Utility.LogAddedEventHandler(Logger_LogAdded), data);
+			}
+			else
+			{
+				Logger_LogAdded(data);
+			}
+		});
+	}
+
+	private void Logger_LogAdded(Logger.LogData data)
+	{
+		LogList.Add(data.ToString());
+	}
+}
